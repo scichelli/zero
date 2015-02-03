60f91c6f9ad18b8e5ac57884bd603282a9b4d3bb
Sharon Cichelli <girlwritescode@gmail.com>
2015-02-02 19:58:45 -0600

    Add a "build from Psake script" target to BuildInstructions, calling out to the project's old Psake build script

--- src/Zero.Build/BuildInstructions.cs
+++ src/Zero.Build/BuildInstructions.cs
@@ -24,6 +24,17 @@
             Log("I am the other thing");
         }
 
+        public void Psake()
+        {
+            //CURRENT build.cmd CALL TO PSAKE:
+            //@echo off
+            //.\src\.nuget\nuget.exe install src\.nuget\packages.config -source "https://nuget.org/api/v2/" -RequireConsent -o "src\packages"
+            //powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\src\packages\psake.4.3.2\tools\psake.ps1' %*; if ($psake.build_success -eq $false) { write-host "Build Failed!" -fore RED; exit 1 } else { exit 0 }"
+
+
+            RunPowerShell(@".\src\packages\psake.4.3.2\tools\psake.ps1");
+        }
+
         private static void DemonstrateFileIO()
         {
             File.WriteAllText(OutputPath + @"\CreatedByNautilus.txt", string.Format("created from Zero {0}", DateTime.Now.ToString()));
