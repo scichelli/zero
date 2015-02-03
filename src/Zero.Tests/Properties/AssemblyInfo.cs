2a98fbd3e19c2b354ec4b9cc1805d361ac50c638
Sharon Cichelli <girlwritescode@gmail.com>
2015-02-02 19:58:02 -0600

    regenerated AssemblyInfo files from build script as year ticked over

--- src/Zero.Tests/Properties/AssemblyInfo.cs
+++ src/Zero.Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Zero.Tests")]
 [assembly: AssemblyConfiguration("Debug")]
 
-[assembly: AssemblyCopyright("Copyright © 2014 Patrick Lioi")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Patrick Lioi")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
